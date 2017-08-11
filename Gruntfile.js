69993a28e578a3152ebc9bcca350c1f9f02859a3
Melissa P <73hl10n@gmail.com>
2017-08-11 13:28:52 -0700

    remove site directory and update gruntfile to reflect this

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 var yaml = require("js-yaml");
 var S = require("string");
 
-var CONTENT_PATH_PREFIX = "site/content";
+var CONTENT_PATH_PREFIX = "./content";
 
 // define the grunt function for cli
 module.exports = function(grunt) {
