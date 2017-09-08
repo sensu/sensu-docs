407aa3d397e14c9b1da7f8e6a618b3420e24a7d7
Melissa P <73hl10n@gmail.com>
2017-09-07 17:23:48 -0700

    remove location refresh, add constant for cookie life

--- static/js/platformDropdowns.js
+++ static/js/platformDropdowns.js
@@ -1,47 +1,54 @@
-    // I've heard that .stopPropogation() is not ideal, potentially needs a rework
-    $( document ).ready(function() {
-        // set toggles for top of page dropdown
-        $('.platformButtonTitle').click( function(event){
-            event.stopPropagation();
-            $('.hiddenPlatforms').toggle();
-        });
-        $(document).click( function(){
-            $('.hiddenPlatforms').hide();
-        });
+const $cookieLife = 1825;
 
-        setTopDropdown();
+// I've heard that .stopPropogation() is not ideal, potentially needs a rework
+$( document ).ready(function() {
+    // set toggles for top of page dropdown
+    $('.platformButtonTitle').click( function(event){
+        event.stopPropagation();
+        $('.hiddenPlatforms').toggle();
+    });
+    $(document).click( function(){
+        $('.hiddenPlatforms').hide();
     });
 
-    function setTopDropdown() {
-      var $platformTextInsert = document.getElementById("platformButtonTitleText");
-      if (Cookies.get("platform")) {
-        $platformTextInsert.innerHTML = Cookies.get("platform") + '<i class="fa fa-chevron-down dropdownArrow" aria-hidden="true"></i>';
-      }
-    }
+    setTopDropdown();
+});
 
-    function setHeaderDropdowns($uniqueness) {
-      // set toggles for under header dropdowns
-      $('.platformButtonTitle-' + $uniqueness).click( function(event){
-        event.stopPropagation();
-        $('.hiddenPlatforms-' + $uniqueness).toggle();
-      });
-      $(document).click( function(){
-        $('.hiddenPlatforms-' + $uniqueness).hide();
-      });
+function setTopDropdown() {
+  var $platformTextInsert = document.getElementById("platformButtonTitleText");
+  if (Cookies.get("platform")) {
+    $platformTextInsert.innerHTML = Cookies.get("platform") + '<i class="fa fa-chevron-down dropdownArrow" aria-hidden="true"></i>';
+  }
+}
 
-      var $platformTextInsert = document.getElementById("platformButtonTitleText-" + $uniqueness);
-      if (Cookies.get("platform")) {
-        $platformTextInsert.innerHTML = Cookies.get("platform") + '<i class="fa fa-chevron-down dropdownArrow-' + $uniqueness + '" aria-hidden="true"></i>';
-      }
-    }
+function setHeaderDropdowns($uniqueness) {
+  // set toggles for under header dropdowns
+  $('.platformButtonTitle-' + $uniqueness).click( function(event){
+    event.stopPropagation();
+    $('.hiddenPlatforms-' + $uniqueness).toggle();
+  });
+  $(document).click( function(){
+    $('.hiddenPlatforms-' + $uniqueness).hide();
+  });
 
-    function setPlatformCookie($platform) {
-      Cookies.set('platform', $platform, { expires: 2030 });
-    }
+  var $platformTextInsert = document.getElementById("platformButtonTitleText-" + $uniqueness);
+  if (Cookies.get("platform")) {
+    $platformTextInsert.innerHTML = Cookies.get("platform") + '<i class="fa fa-chevron-down dropdownArrow-' + $uniqueness + '" aria-hidden="true"></i>';
+  }
+}
 
-    function setPlatformCookie($platform, $product, $title, $parent) {
-      Cookies.set('platform', $platform, { expires: 2030 });
-      location.reload();
-      document.getElementById($parent).scrollIntoView();
-    }
+function setPlatformCookie($platform) {
+  Cookies.set('platform', $platform, { expires: $cookieLife });
+}
+
+function setPlatformCookie($platform, $product, $title) {
+  Cookies.set('platform', $platform, { expires: $cookieLife });
+  togglePlatformDivs();
+  setTopDropdown();
+  $platform = $platform.replace("/", "_");
+  $platform = $platform.replace(" ", "-");
+  var dropdown = document.getElementById("platformButtonTitleText-" + $platform);
+  dropdown.innerHTML = Cookies.get("platform") + '<i class="fa fa-chevron-down dropdownArrow-' + $platform + '" aria-hidden="true"></i>';
+  dropdown.scrollIntoView();
+}
 
