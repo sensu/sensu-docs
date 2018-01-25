53e1e9f00bfa922382dd47507d78d1ba554be062
Justin Kolberg <amd.prophet@gmail.com>
2018-01-24 23:41:06 -0800

    re-add solaris postinst.sh
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

---
+++ static/files/postinst.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# add sensu group
+set +e
+grep "^sensu:" /etc/group > /dev/null
+RC=$?
+set -e
+if [ "${RC}" != 0 ]; then
+    groupadd sensu
+fi
+
+# add sensu group
+set +e
+grep "^sensu:" /etc/passwd > /dev/null
+RC=$?
+set -e
+if [ "${RC}" != 0 ]; then
+    useradd -d /opt/sensu -g sensu sensu
+    passwd -N sensu
+fi
+
+# chown sensu dirs
+chown -Rh 0:0 /opt/sensu
+chown -R sensu:sensu /etc/sensu
+chown -R sensu:sensu /var/log/sensu
+mkdir -p /var/run/sensu
+chown -R sensu:sensu /var/run/sensu
+chown -R sensu:sensu /var/cache/sensu
