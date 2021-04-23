94a136b7c37d8f8adafddc1198ebd9ff1bc256ec
Hillary Fraley <hillary@sensu.io>
2021-04-23 09:05:23 -0500

    Move copy tracking to separate function to try to resolve newline error

--- static/js/clip.js
+++ static/js/clip.js
@@ -1,3 +1,4 @@
+
 /*
  * clipboard for hugo
  */
@@ -16,27 +17,24 @@
         addCopy($codes[i]);
     }
 
+
     var clipboard = new ClipboardJS('.copy', {
         target: function(trigger) {
             return trigger.previousElementSibling;
         }
     });
-
-    /*
-    * sends clicks on buttons with .copy class as a Google Analytics event
-    */
-
-    (function() {
-        const pageURL = document.location.pathname + document.location.search;
-
-        var button = document.getElementsByClassName("copy");
-        button.addEventListener("click", function() {
-            ga('send','event','Clipboard','Code copied',pageURL);
-        });
-    })();
-
 })(document, Clipboard);
 
 
+/*
+* sends clicks on clipboard buttons with .copy class as Google Analytics events
+*/
 
+(function() {
+    const pageURL = document.location.pathname + document.location.search;
 
+    var button = document.getElementsByClassName("copy");
+    button.addEventListener("click", function() {
+        ga('send','event','Clipboard','Code copied',pageURL);
+    });
+})();
