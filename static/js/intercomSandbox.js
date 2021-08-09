7df40944e85807b7eaadaff4d78afef49a1de6a2
Hillary Fraley <hillary@sensu.io>
2021-08-09 14:50:47 -0500

    Update intercom javascript to use correct app ID

--- static/js/intercomSandbox.js
+++ static/js/intercomSandbox.js
@@ -2,11 +2,11 @@
  * code for intercom chat window for https://docs.sensu.io/sensu-go/x.x/learn/learn-sensu-sandbox/
  */
 
-var APP_ID = "docsSandboxPage";
+var APP_ID = "abr73apk";
 
 window.intercomSettings = {
-    app_id: APP_ID,
-};
+    app_id: "abr73apk"
+  };
 (function () {
     var w = window;
     var ic = w.Intercom;
@@ -27,13 +27,11 @@ window.intercomSettings = {
             var s = d.createElement("script");
             s.type = "text/javascript";
             s.async = true;
-            s.src = "https://widget.intercom.io/widget/" + APP_ID;
+            s.src = "https://widget.intercom.io/widget/abr73apk";
             var x = d.getElementsByTagName("script")[0];
             x.parentNode.insertBefore(s, x);
         };
-        if (document.readyState === "complete") {
-            l();
-        } else if (w.attachEvent) {
+        if (w.attachEvent) {
             w.attachEvent("onload", l);
         } else {
             w.addEventListener("load", l, false);
