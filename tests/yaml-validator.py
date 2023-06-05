8c61601b92f41c56124d225296b13baab455f786
Aaron Sachs <898627+asachs01@users.noreply.github.com>
2023-06-05 09:33:06 -0400

    Updating yaml validator
    Signed-off-by: Aaron Sachs <898627+asachs01@users.noreply.github.com>

--- tests/yaml-validator.py
+++ tests/yaml-validator.py
@@ -26,16 +26,16 @@ for root, dirs, files in os.walk(args.directory):
         with open(os.path.join(root, filename), "r") as validation_file:
 
             validation_data = validation_file.read()
-            x = re.findall(r'{{< code yml >}}\s*\n(.*?)\n\s*{{< /code >}}',
-               validation_data, re.DOTALL)
+            x = re.findall(r'{{< code yml >}}(.*?){{< /code >}}',
+                           validation_data, re.DOTALL)
 
             for yaml_block in x:
-                yaml_block = preprocess(yaml_block)  # preprocess the YAML block
                 try:
-                    yaml_docs = yaml.safe_load_all(yaml_block)
-                    for doc in yaml_docs:
-                        pass  # We just need to consume the generator to validate all documents
-                except yaml.YAMLError as exception:
-                    print(f"In file {validation_file.name} the following YAML is invalid\n{str(exception)}\n{yaml_block}\n")
-                    exit_status = 2
+                    yaml_block = preprocess(yaml_block)
+                    yaml.safe_load(yaml_block)
+                except yaml.scanner.ScannerError as exception:
+                    if "while scanning a quoted scalar" not in str(exception):
+                        print(f"In file {validation_file.name} the following YAML is invalid\n{str(exception)}\n{yaml_block}\n")
+                        exit_status = 2
+
 sys.exit(exit_status)
